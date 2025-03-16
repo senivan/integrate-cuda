b20f255931c428758b0c3a381a75ce541b156b27
Ivan Sen <sen.pn@ucu.edu.ua>
2025-03-16 16:28:50 +0200

    Init

---
+++ include/integratec.cuh
@@ -0,0 +1,8 @@
+#define KERNEL_INCLUDE
+#ifndef KERNEL_INCLUDE
+__device__ float function_1();
+__device__ float function_2();
+__device__ float function_3();
+
+__global__ void integrate(float x_start, float x_end, flaot y_start, float y_end, int s)
+#endif
